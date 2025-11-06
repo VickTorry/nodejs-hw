13df6ca2877a6a8d9ce9f3697026d4bc724a5bb6
VickTorry <victoriadudnyk@gmail.com>
2025-11-06 11:53:24 -0500

    added note validation with celebrate

--- src/server.js
+++ src/server.js
@@ -2,46 +2,36 @@
 
 import express from 'express';
 import cors from "cors";
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
-import { Note } from './models/note.js';
+// import { Note } from './models/note.js';
+import { errors } from "celebrate";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(notesRoutes);
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    "message": "Retrieved all notes",
-  });
-});
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    "message": `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-
-app.get('/__diag', async (req, res, next) => {
-  try {
-    const { host, name, readyState } = mongoose.connection;
-    const count = await Note.estimatedDocumentCount(); // in current DB
-    res.json({ host, dbName: name, readyState, collection: Note.collection.name, count });
-  } catch (e) { next(e); }
-});
+// app.get('/__diag', async (req, res, next) => {
+//   try {
+//     const { host, name, readyState } = mongoose.connection;
+//     const count = await Note.estimatedDocumentCount(); // in current DB
+//     res.json({ host, dbName: name, readyState, collection: Note.collection.name, count });
+//   } catch (e) { next(e); }
+// });
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,63 @@
+
+import { Joi, Segments } from 'celebrate';
+import { TAGS } from '../contacts/tags.js';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1).required(),
+    perPage: Joi.number().integer().min(5).max(20).default(10).required(),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow('').default(''),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().trim().min(1).required().messages({
+      "string.base": "Title must be a string",
+      "string.min": "Title should have at least {#limit} characters",
+      'any.required': 'Title is required',
+      'string.empty': 'Title cannot be empty',
+    }),
+    content: Joi.string().allow('').default('').messages({
+      "string.base": "Content must be a string",
+    }),
+    tag: Joi.string().valid(...TAGS).default('Todo').messages({
+      "string.base": "Tag must be a string",
+      "any.only": `Tag must be one of the following: ${TAGS.join(', ')}`,
+    }),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().trim().min(1).messages({
+      "string.base": "Title must be a string",
+      "string.min": "Title should have at least {#limit} characters",
+      'string.empty': 'Title cannot be empty',
+    }),
+    content: Joi.string().allow('').messages({
+      "string.base": "Content must be a string",
+    }),
+    tag: Joi.string().valid(...TAGS).messages({
+      "string.base": "Tag must be a string",
+      "any.only": `Tag must be one of the following: ${TAGS.join(', ')}`,
+    }),
+  }).min(1).messages({
+      'object.min': 'At least one field must be provided for update',
+    }),
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
